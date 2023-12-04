127ef74cb7c6600c392e270564380c293edd8d4b
Ismael <ismael.bortoluzzi77@gmail.com>
2023-12-04 11:11:21 -0300

    creating code to evaluate each statement separated by ;

--- Main.hs
+++ Main.hs
@@ -5,6 +5,23 @@ import Parser
 import TypeChecker
 import Interpreter
 
-main = getContents >>= print . eval . typecheck . parser . lexer 
+
+splitOn :: (Char -> Bool) -> String -> [String]
+splitOn p s = case dropWhile p s of
+                      "" -> []
+                      s' -> w : splitOn p s''
+                            where (w, s'') = break p s'
+    
+
+evaluate :: String -> [Expr]
+evaluate lines = execStatement $ splitOn (==';') lines
+
+
+execStatement :: [String] -> [Expr]
+execStatement [] = []
+execStatement (line:lines) = (eval $ typecheck $ parser $ lexer line) : execStatement lines
+
+
+main = getContents >>= print . evaluate
 
 -- cat a.in | runghc Main.hs
